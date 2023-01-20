367f3513a740fd0d4652cb1b933f4a8facd87eed
ts-hanjaya.suryalim <ts-hanjaya.suryalim@rakuten.com>
2023-01-21 00:59:21 +0900

    recursive v1.1.0

--- recursive.go
+++ recursive.go
@@ -1,8 +1,14 @@
 package recursive
 
+import "fmt"
+
 func Recursive(number int) int {
 	if number == 1 {
 		return 1
 	}
 	return number * Recursive(number-1)
+}
+
+func Hello(name string) string {
+	return fmt.Sprintf("Hello %s", name)
 }
--- recursive_test.go
+++ recursive_test.go
@@ -9,3 +9,11 @@ func TestRecursive(t *testing.T) {
 		t.Errorf("expected %d, got %d",expected, result)
 	}
 }
+
+func TestHello(t *testing.T) {
+	expected := "Hello Jay"
+	result := Hello("Jay")
+	if expected != result {
+		t.Errorf("expected %s, got %s", expected, result)
+	}
+}
